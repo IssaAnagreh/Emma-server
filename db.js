a0627dbd4ce4aa5543bc8e708cfa042d78df76f8
IssaAnagreh <isa_anagreh@hotmail.com>
2022-03-10 16:12:55 +0300

    add err catching to db connection

--- db.js
+++ db.js
@@ -6,7 +6,10 @@ const dbHost = process.env.DB_HOST;
 const dbName = process.env.DB_NAME;
 
 const client = new MongoClient(dbHost, { useUnifiedTopology: true });
-client.connect(function (err) {
+client.connect((err) => {
+  if (err) {
+    console.log("error in connection to DB", err);
+  }
   console.log(`Connected successfully to ${dbName} Database`);
 });
 
